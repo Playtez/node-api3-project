d0bd42c2fa227356b79e4a75bc34c5121edf6cf3
Aaron Pleitez <aaron.pleitez12@gmail.com>
2020-01-16 00:11:19 -0600

    MVP

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const express = require('express');
+const express = require("express");
+
+// const postRouter = require("./posts/postRouter");
+const userRouter = require("./users/userRouter");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use("/api/users", logger, userRouter);
+
+server.get("/", logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  const { method, originalUrl } = req;
+  console.log(`${method} to ${originalUrl}`, Date.now());
+  next();
+}
 
 module.exports = server;
